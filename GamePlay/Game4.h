fb21c30220e5b7686deabcc36d2d15a93ad3e724
Marija <marijaOSI2018@hotmail.com>
2019-01-02 19:05:59 +0100

    Header sa klasama i nekim funkcijama za realizaciju 4. igre

---
+++ GamePlay/Game4.h
@@ -0,0 +1,47 @@
+#pragma once
+#define DIMENSION 10
+#define MINENUM 20
+
+class Mineboard
+{
+public:
+	Mineboard();
+	~Mineboard();
+
+	
+	void uncover_square(int, int);// setting revealed status of said square and neighbour squares if needed
+	int how_many_left() const { return squaresLeft; } // returns number of uncovered squares
+	
+	
+
+private:
+
+	class Square
+
+	{
+	public:
+		enum class State { Hidden, Revealed };
+		void put_mine();
+		bool has_mine() const { return isMine; }
+		
+
+	private:
+		bool isMine = false;
+		State state = State::Hidden; 
+		char revealedState; //if mine is uncovered constains number of neighbour mines  or blank if it doesen't hold information
+
+	};
+
+
+	static constexpr int width = DIMENSION;
+	static constexpr int height = DIMENSION;
+	unsigned squaresLeft = width * height; //initial value, will be decremented
+	Square board[width*height];
+
+
+	
+public:
+	Square& square_at(int, int); //used for accessing the square at given coordinates
+
+};
+
