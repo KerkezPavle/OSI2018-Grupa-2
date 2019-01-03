acef7d90c9508fb532fa8c07e9d4a3f6c82ac04c
Marija <marijaOSI2018@hotmail.com>
2019-01-03 13:36:51 +0100

    Added functions for drawing a single square and the board onto the screen, fixed issues in previously implemented functions

--- GamePlay/Game4.cpp
+++ GamePlay/Game4.cpp
@@ -1,6 +1,7 @@
 #include "Game4.h"
 #include <iostream>
 #include <random>
+#include <iomanip>
 
 Mineboard::Mineboard()
 {
@@ -27,9 +28,31 @@ Mineboard::~Mineboard()
 {
 }
 
+void Mineboard::draw_board(bool should_reveal)
+{
+	char LINE[41] = "----------------------------------------";
+	std::cout << CLEAR << "   ";
+	// drawing the board axis
+	for (size_t j = 0; j < width; ++j)
+		std::cout << std::setw(3) << j << ' ';
+	std::cout << "\n";
+
+	for (size_t i = 0; i < height; ++i) {
+		std::cout << "   " << LINE << '\n'
+			<< std::setw(2) << i << " |";
+		for (size_t j = 0; j < width; ++j)
+		{
+			square_at(i, j).draw_square(should_reveal);
+			std::cout << RESET << '|';
+		}
+		std::cout << "\n";
+	}
+	std::cout << "   " << LINE << '\n';
+}
+
 void Mineboard::uncover_square(int row, int col)
 {
-	if (index_in_range(row, col) && square_at(row, col).state == Square::State::Hidden)
+	if (index_in_range(row, col) && square_at(row, col).state == Square::State::Hidden && !square_at(row, col).has_mine())
 
 	{
 		squaresLeft--;
@@ -37,9 +60,12 @@ void Mineboard::uncover_square(int row, int col)
 		for (int i = -1; i < 2; i++)
 			for (int j = -1; j < 2; j++)
 				nearMinesCount += square_at(row + i, col + j).has_mine() ? 1 : 0;
-		square_at(row, col).set_revealed_state(nearMinesCount);
 
-		if (nearMinesCount) 
+		square_at(row, col).make_revealed();
+		square_at(row, col).set_close_mines(nearMinesCount);
+
+
+		if (!nearMinesCount) 
 		{
 			uncover_square(row - 1, col);
 			uncover_square(row, col - 1);
@@ -53,7 +79,7 @@ void Mineboard::uncover_square(int row, int col)
 
 Mineboard::Square& Mineboard::square_at(int row, int col)
 {
-	return board[width *row + col];
+	return board[row][col];
 }
 
 bool Mineboard::index_in_range(int row, int col)
@@ -67,8 +93,25 @@ bool Mineboard::index_in_range(int row, int col)
 	}
 }
 
-void Mineboard::Square::set_revealed_state(int count)
+void Mineboard::Square::set_close_mines(int count)
 {
-	 if (count == 0) revealedState = ' '; 
-	 else revealedState = count + 48; 
+	 if (count == 0) closeMines = ' ' - 48; 
+	 else closeMines = count; 
+}
+
+void Mineboard::Square::draw_square(bool should_reveal) const
+{
+
+	if (has_mine() && should_reveal)
+		std::cout << RED << ' ' << '#' << ' ';
+	else if (state == State::Revealed && !has_mine())
+	{
+		std::cout << GREEN << ' ' << (char)(closeMines+48) << ' ';
+	}
+
+	else
+		std::cout << TILE << ' ' << '+' << ' ';
+
+
+
 }
