a1ff8d46aa37d5279a8d7d00a5df37817d2f9b55
Желимир Малетић <44443282+zelimirmaletic@users.noreply.github.com>
2019-01-05 13:53:50 +0100

    Minor function changes
    -Upgraded functions in order to ensure valid data input.
    -Game output redesigned.
    -Minor bug fixes.

--- GamePlay/loto.h
+++ GamePlay/loto.h
@@ -3,81 +3,95 @@
 
 #define NUMBER 7
 #define RANDNUMBER 20
-#include <iostream>
 #include <random>
+#include <iostream>
 
 
-bool isInArray(int *array, int size, int key)
+bool isInArray(int *array,int size, int key)
 {
-	for (int i = 0; i < size; i++)
-		if (array[i] == key)
-			return true;
-	return false;
+    for(int i=0;i<size;i++)
+        if(array[i]==key)
+            return true;
+    return false;
 }
 void printCombination(int *array, int size)
 {
-	std::cout << "[ ";
-	for (int i = 0; i < size; i++)
-		std::cout << array[i] << " ";
-	std::cout << "]" << std::endl;
+    std::cout<<"[ ";
+    for(int i=0;i<size;i++)
+        std::cout<<array[i]<<" ";
+    std::cout<<"]"<<std::endl;
 }
 void loadUserCombination(int *combinationArray)
 {
-	std::cout << "Unesite vasu kombinaciju:" << std::endl;
-	int tmp = 0;
-	for (int i = 0; i < NUMBER; i++)
-	{
-		do
-		{
-			std::cout << "\t" << i + 1 << ". broj--->";
-			std::cin >> tmp;
-		} while (tmp <= 0 || tmp > 45 || isInArray(combinationArray, NUMBER, tmp));
-		combinationArray[i] = tmp;
-	}
+    std::cout<<"Unesite vašu kombinaciju:"<<std::endl;
+    int tmp=-1;
+    int errorFlag=0;
+    for(int i=0;i<NUMBER;i++)
+    {
+        do
+        {
+            std::cout<<"\t"<<i+1<<". broj--->";
+            char c ;
+            if( !( std::cin >>tmp ) || ( std::cin.get(c) && !std::isspace(c) ) )
+            {
+                std::cin.clear();
+                std::cin.ignore( 500, '\n' );
+                tmp = -1 ;
+            }
+        }while(tmp<0 || tmp>45 || isInArray(combinationArray,NUMBER,tmp));
+        combinationArray[i]=tmp;
+    }
 }
 void generateRandomCombination(int *randomCombination)
 {
-	std::random_device randomGenerator;
-	for (int i = 0; i < RANDNUMBER; ++i) {
-		int tmp;
-		do
-		{
-			tmp = 1 + abs((int)randomGenerator() % 45);
-		} while (isInArray(randomCombination, RANDNUMBER, tmp));
-		randomCombination[i] = tmp;
-	}
+    std::random_device randomGenerator;
+    for (int i = 0; i < RANDNUMBER; ++i) {
+        int tmp;
+        do
+        {
+            tmp =  1 + abs((int) randomGenerator() % 45);
+        }while(isInArray(randomCombination,RANDNUMBER,tmp));
+        randomCombination[i]=tmp;
+    }
 }
 int calculateScore(int *userCombination, int *randomCombination)
 {
-	int finalScore = 0;
-	int counter = 1;
-	for (int i = 0; i < NUMBER; i++)
-		if (isInArray(randomCombination, RANDNUMBER, userCombination[i]))
-		{
-			finalScore += counter * 10;
-			counter++;
-		}
-	return finalScore;
+    int finalScore = 0;
+    int counter = 1;
+    for(int i=0;i<NUMBER;i++)
+        if(isInArray(randomCombination,RANDNUMBER,userCombination[i]))
+        {
+            finalScore+=counter*10;
+            counter++;
+        }
+    return finalScore;
 }
 void lotoGame(void)
 {
-	int userScore = 0;
-	int *userCombination = new int[NUMBER];
-	int *randomCombination = new int[RANDNUMBER];
-	loadUserCombination(userCombination);
-	printCombination(userCombination, NUMBER);
-	do {
-		generateRandomCombination(randomCombination);
-		userScore = calculateScore(userCombination, randomCombination);
-	} while (userScore > 60);
-	printCombination(randomCombination, RANDNUMBER);
+    int userScore = 0;
+    int *userCombination = new int[NUMBER];
+    int *randomCombination = new int[RANDNUMBER];
+    std::cout<<"-------------------------------------------------------------"<<std::endl;
+    std::cout<<"|                           BINGO                           |"<<std::endl;
+    std::cout<<"-------------------------------------------------------------"<<std::endl;
 
-	std::cout << std::endl;
-	std::cout << "OSVOJENI BODOVI: " << userScore << std::endl;
+    loadUserCombination(userCombination);
+    do{
+        generateRandomCombination(randomCombination);
+        userScore = calculateScore(userCombination,randomCombination);
+    }while(userScore>60);
 
-	std::cout << std::endl;
-	delete[] userCombination;
-	delete[] randomCombination;
+    std::cout<<"-------------------------------------------------------------"<<std::endl;
+    std::cout<<"VAŠA KOBINACIJA: "; printCombination(userCombination,NUMBER);
+    std::cout<<"-------------------------------------------------------------"<<std::endl;
+    std::cout<<"DOBITNA KOBINACIJA: "<<std::endl; printCombination(randomCombination,RANDNUMBER);
+    std::cout<<"-------------------------------------------------------------"<<std::endl;
+    std::cout<<"OSVOJENI BODOVI: "<< userScore << std::endl;
+    std::cout<<"-------------------------------------------------------------"<<std::endl;
+    std::cout<<"|                           KRAJ                           |"<<std::endl;
+    std::cout<<"-------------------------------------------------------------"<<std::endl;
+    delete[] userCombination;
+    delete[] randomCombination;
 }
 
 
